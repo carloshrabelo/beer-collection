2e78c87f5e9f10df40e3ff0bcb8438cd674dd511
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2023-11-28 01:13:57 +0000

    fix: styled className cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: "images.punkapi.com" }] },
+  compiler: {
+    styledComponents: {
+      displayName: false,
+    },
+  },
 };
 
 module.exports = nextConfig;
